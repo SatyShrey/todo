79a88a8e413b8c57dcc883a630d6901f058a5e9f
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-04-21 04:44:19 +0530

    Add files via upload

--- api.js
+++ api.js
@@ -6,8 +6,11 @@ app.use(cors({}))
 app.use(urlencoded({extended:true}))
 app.use(json())
 
+import { config } from 'dotenv';
+config();
+
 import { MongoClient as mongoClient } from "mongodb"
-let conStr='mongodb+srv://sndsatya:QtAy7QbfwCnzUhvu@clustersnd.adfao0n.mongodb.net'
+let conStr=process.env.url
 
 mongoClient.connect(conStr).then(clientObject => {
     const database=clientObject.db('todo');
@@ -17,22 +20,30 @@ mongoClient.connect(conStr).then(clientObject => {
     })
 
     app.post('/add', (req, res) => {
-        database.collection('notes').insertOne(req.body);
-        res.end();
+        const note=req.body
+        database.collection('notes').insertOne(req.body).then(()=>{
+            database.collection('notes').find({email:note.email}).toArray().then(notes=>{
+                res.send(notes)
+                res.end()
+            })
+        })
     })
 
-    app.get('/notes/:id',(req,res)=>{
-        database.collection('notes').find({email:req.params.id}).toArray().then(notes=>{
+    app.post('/notes',(req,res)=>{
+        database.collection('notes').find(req.body).toArray().then(notes=>{
             res.send(notes)
             res.end()
         })
     })
 
-    app.delete('/remove/:id/:email', (req, res) => {
-        var id = req.params.id;
-        var email=req.params.email
-        database.collection('notes').deleteOne({ id: id,email:email });
-        res.send("Deleted")
+    app.delete('/remove/:email/:id', (req, res) => {
+        const note={id:req.params.id,email:req.params.email}
+        database.collection('notes').deleteOne(req.body).then(()=>{
+            database.collection('notes').find({email:note.email}).toArray().then(notes=>{
+                res.send(notes)
+                res.end()
+            })
+        })
     })
 
 })
